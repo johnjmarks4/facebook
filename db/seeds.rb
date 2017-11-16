f77cd10b79ff4c4b99b59440978aa66ac6a44d06
John Marks <johnjmarks4@gmail.com>
2017-11-16 17:21:35 -0600

    Populate database with faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
 require 'faker'
 
-User.create(first_name: Faker::Name.first_name, 
-            last_name:  Faker::Name.last_name,
-            email:      Faker::Internet.email)
+20.times do
+  User.create(first_name:            Faker::Name.first_name, 
+              last_name:             Faker::Name.last_name, 
+              email:                 Faker::Internet.email, 
+              password:              "password", 
+              password_confirmation: "password")
+end
 
-User.new(:password => password).encrypted_password
+ids = []
+i = -1
+
+User.all.each do |user|
+  ids << user.id
+end
+
+ids.shuffle.each do |id|
+  Friendship.create(friend_id: id, user_id: ids[i])
+  i -= 1
+end
