bbc8387babc7d2a0df69aedf9e3c40541014015e
John Marks <johnjmarks4@gmail.com>
2017-12-04 22:39:55 -0600

    refactor refresh_notifications method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,29 +11,11 @@ class ApplicationController < ActionController::Base
   def refresh_notifications
     session[:notifications] = { } if !session[:notifications]
     session[:notifications][:likes] = [] if !session[:notifications][:likes]
-    @friend_requests = []
-    @likes = []
 
-    # Refactor these into seperate methods
     if current_user
       @friendship = Friendship.new
-      session[:notifications][:friend_requests] = FriendRequest.refresh_friend_requests(current_user)
-      session[:notifications][:friend_requests].each do |request|
-        user = User.find(request.user_id)
-        name = user.first_name + " " + user.last_name
-        @friend_requests << ["#{name} added you", request.user_id]
-      end
-
-      new_likes = Like.refresh_likes(current_user, session[:notifications][:likes])
-      session[:notifications][:likes] << new_likes
-      if !new_likes.empty?
-        new_likes.each do |like|
-          like = like.first
-          user = User.find(like.user_id)
-          name = user.first_name + " " + user.last_name
-          @likes << "#{name} liked your #{view_context.link_to 'post', posts_show_path(like.user_id)}".html_safe
-        end
-      end
+      @friend_requests = get_friend_requests
+      @likes = get_likes
     end
   end
 
@@ -43,4 +25,27 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if !session[:user_id].nil?
   end
   helper_method :current_user
+
+  def get_friend_requests
+    session[:notifications][:friend_requests] = FriendRequest.refresh_friend_requests(current_user)
+    session[:notifications][:friend_requests].map do |request|
+      user = User.find(request.user_id)
+      name = user.first_name + " " + user.last_name
+      ["#{name} added you", request.user_id]
+    end
+  end
+
+  def get_likes
+    new_likes = Like.refresh_likes(current_user, session[:notifications][:likes])
+    session[:notifications][:likes] << new_likes
+
+    if !new_likes.empty?
+      new_likes.map do |like|
+        like = like.first
+        user = User.find(like.user_id)
+        name = user.first_name + " " + user.last_name
+        "#{name} liked your #{view_context.link_to 'post', posts_show_path(like.user_id)}".html_safe
+      end
+    end
+  end
 end
