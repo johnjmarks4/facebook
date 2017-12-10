40185e4cfaac131b7e301a047c9b8acb03c754f2
John Marks <johnjmarks4@gmail.com>
2017-12-09 20:23:57 -0600

    Add gravatar default image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,10 +2,10 @@ module UsersHelper
 
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    default_url = "#{root_url}images/guest.png"
+    default_url = "http://www.gravatar.com/avatar"
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=250&d=#{CGI.escape(default_url)}"
     user_name = "#{user.first_name} #{user.last_name}"
-    image_tag(gravatar_url, alt: user_name, class: "gravatar")
+    image_tag(gravatar_url, class: "gravatar")
   end
 
   def find_user_by_id(id)
