3354606c8e0f02bffc84cccac638ae403b6fe2e5
John Marks <johnjmarks4@gmail.com>
2017-12-16 19:00:36 -0600

    Tweak size of display name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
+         :recoverable, :rememberable, :trackable,
          :omniauthable, :omniauth_providers => [:facebook]
   has_many :friendships, :dependent => :destroy       
   has_many :friends, :through => :friendships, :dependent => :destroy
@@ -11,6 +11,16 @@ class User < ApplicationRecord
   has_many :posts, :dependent => :destroy
   has_many :comments, :dependent => :destroy
   has_many :likes, :dependent => :destroy
+
+  validates :first_name, presence: true, length: { maximum: 12 }
+  validates :last_name, presence: true, length: { maximum: 12 }
+  validates :email, presence: true
+
+  validates_uniqueness_of    :email,     :case_sensitive => false, :allow_blank => false, :if => :email_changed?
+  validates_format_of    :email,    :with  => Devise.email_regexp, :allow_blank => false, :if => :email_changed?
+  validates_presence_of    :password, :on=>:create
+  validates_confirmation_of    :password, :on=>:create
+  validates_length_of :password, :within => Devise.password_length, :allow_blank => false
 
   def self.users_matching_name(name)
     first_name = name.split(" ")[0]
