c64c0c607c04cd4faa963f8716783744ac9ddc5b
John Marks <johnjmarks4@gmail.com>
2017-12-22 16:20:43 -0600

    Add signed_in? before action to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery# with: :exception
   before_action :authenticate_user!
+  before_action :signed_in?
   before_action :refresh_notifications
 
   def after_sign_in_path_for(resource_or_scope)
@@ -20,6 +21,15 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def signed_in?(scope=nil)
+    [scope || Devise.mappings.keys].flatten.any? do |_scope|
+      if !warden.authenticate?(scope: _scope)
+        flash[:notice] = "You must be signed in to view that page"
+        redirect_to root_path 
+      end
+    end
+  end
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if !session[:user_id].nil?
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   skip_before_action :refresh_notifications
+  skip_before_action :signed_in?, only: [:new]
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -58,14 +59,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-
-  private 
-  
-  def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-  end
-
-  def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
-  end
 end
